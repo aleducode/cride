833b04f51c6acaae8dddafae2d5487c9c0342efe
Alejandro Duque <ducode@outlook.com>
2019-05-19 00:22:24 -0300

    many to many relation between users and circles

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -1,40 +1,46 @@
 """Circle model"""
 
-#Django 
+#Django
 from django.db import models
 
 #Utilities
 from cride.utils.models import CRideModel
 
+
 class Circle(CRideModel):
     """circle model
     A circle is a private group where rides are offered and taken
-    by its members. To join a circle a user must be receive an unique 
+    by its members. To join a circle a user must be receive an unique
     invitation code from an existing mcircle member
     """
 
-    name=models.CharField('circle name', max_length=140)
-    slug_name=models.SlugField(unique=True,max_length=40)
+    name = models.CharField('circle name', max_length=140)
+    slug_name = models.SlugField(unique=True, max_length=40)
 
-    about=models.CharField('circle description', max_length=255)
-    picture=models.ImageField(upload_to='circles/picture',blank=True,null=True)
+    about = models.CharField('circle description', max_length=255)
+    picture = models.ImageField(upload_to='circles/picture', blank=True, null=True)
 
-    #stats
-    rides_offered= models.PositiveIntegerField(default=0)
-    rides_taken= models.PositiveIntegerField(default=0)
+    members = models.ManyToManyField(
+        'users.User', through='circles.Membership',
+        through_fields=('circle', 'user')
+        )
 
-    verified= models.BooleanField(
+    # stats
+    rides_offered = models.PositiveIntegerField(default=0)
+    rides_taken = models.PositiveIntegerField(default=0)
+
+    verified = models.BooleanField(
         'verified circle',
         default=0,
         help_text='verified circles are also known as official communities.'
     )
 
-    is_public =models.BooleanField(
+    is_public = models.BooleanField(
         default=True,
         help_text='public circles ar listed in the main page so everyone know about their existance.'
     )
 
-    is_limited=models.BooleanField(
+    is_limited = models.BooleanField(
         'limited',
         default=0,
         help_text='limited circles can grow up to a fixed number of members'
@@ -44,13 +50,11 @@ class Circle(CRideModel):
         help_text='If circle is limited, this will be the limit of the number of members'
 
     )
-    
+
     def __str__(self):
         """Return circles name"""
         return self.name
-    
-    #override class meta from Cridemodel
+
+    # override class meta from Cridemodel
     class Meta(CRideModel.Meta):
-        ordering =['-rides_taken','-rides_offered']
-    
-    
+        ordering = ['-rides_taken', '-rides_offered']
---
+++ cride/circles/models/memberships.py
@@ -0,0 +1,45 @@
+"""Membership model"""
+
+# Django
+from django.db import models
+
+# Utilities
+from cride.utils.models import CRideModel
+
+
+class Membership(CRideModel):
+    """Membership model
+
+    is the table thats holds the relationship
+    between a user and a circle.
+    """
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    circle = models.ForeignKey('circles.Circle', on_delete=models.CASCADE)
+
+    is_public = models.BooleanField(
+        default=True,
+        help_text='circle admins can update the circles data and manage its members.'
+    )
+    used_invitations = models.PositiveSmallIntegerField(default=0)
+    remaining_invitation = models.PositiveSmallIntegerField(default=0)
+    invited_by = models.ForeignKey(
+        'users.User',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='invited_by'
+    )
+
+    # stats
+    rides_offered = models.PositiveIntegerField(default=0)
+    rides_taken = models.PositiveIntegerField(default=0)
+
+    # Status
+    is_active = models.BooleanField(
+        default=True,
+        help_text='only active user are allow to interact in the circle.'
+    )
+
+    def __str__(self):
+        """Return username and circle"""
+        return '@{} at #'.format(self.user.username, self.circle.slug_name)
